585569f2dfac3f668e3d14027c85e00c98ee8a2d
sjdodge123 <sjdodge@outlook.com>
2017-06-26 13:59:52 -0400

    updated deploy pattern

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,8 @@
 set "curpath=%cd%"
 winscp /script=deployScript.txt
+plink ubuntu@ec2-34-228-10-17.compute-1.amazonaws.com sudo rm -rf /var/www/html/
+plink ubuntu@ec2-34-228-10-17.compute-1.amazonaws.com sudo rsync -a /home/ubuntu/cadygorbet.com/. /var/www/html/
+
+
+
 
