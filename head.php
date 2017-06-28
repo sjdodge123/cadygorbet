ac55c909cb03de076a426ed1a267d8dab3047799
sjdodge123 <sjdodge@outlook.com>
2017-06-27 22:10:08 -0400

    google ads

--- head.php
+++ head.php
@@ -5,5 +5,14 @@
     <link rel='stylesheet' type='text/css' href='css/main.css'>
     <link href="https://fonts.googleapis.com/css?family=News+Cycle|Rokkitt" rel="stylesheet">
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-3665896518813346",
+	    enable_page_level_ads: true
+	  });
+	</script>
+    
     <title>Cady Gorbet</title>
 </head>
