0599018ce77775c11253a617a11d5ef0f79c3572
CADY-DESKTOP\Cady <cady>
2017-07-07 14:06:21 -0400

    staged header

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <meta charset='utf-8'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel='stylesheet' type='text/css' href='css/bootstrap.css'>
 <link rel='stylesheet' type='text/css' href='css/main.css'>
 <link href="https://fonts.googleapis.com/css?family=News+Cycle|Rokkitt" rel="stylesheet">
