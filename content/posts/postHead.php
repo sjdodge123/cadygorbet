bd8a3613135e835d6260c71ab15890b0b2ec4d10
CADY-DESKTOP\Cady <cady>
2017-07-07 13:29:34 -0400

    changed site anchor color to a different shade of pink added favicon

--- content/posts/postHead.php
+++ content/posts/postHead.php
@@ -6,6 +6,7 @@
     <link rel='stylesheet' type='text/css' href='../../../css/main.css'>
     <link href="https://fonts.googleapis.com/css?family=News+Cycle|Rokkitt" rel="stylesheet">
     <link rel="stylesheet" href="../../../font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/x-icon" href="../../../img/favicon.ico" />
     <?php include_once("../../../analyticstracking.php") ?>
     <title>Cady Gorbet</title>
 </head>
--- head.php
+++ head.php
@@ -4,7 +4,7 @@
     <link rel='stylesheet' type='text/css' href='css/main.css'>
     <link href="https://fonts.googleapis.com/css?family=News+Cycle|Rokkitt" rel="stylesheet">
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 	  (adsbygoogle = window.adsbygoogle || []).push({
